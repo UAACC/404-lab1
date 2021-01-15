4863bd29c6d20798ba4567ecb12a1c1edb14a7dd
UAACC <ldh1998321@gmail.com>
2021-01-14 17:24:29 -0700

    update

--- q2.py
+++ q2.py
@@ -1,3 +1,9 @@
 import requests as rq
 
-print(rq.__version__)
+print('The requests version in this environment is ', rq.__version__)
+
+
+hp = rq.get('http://www.google.com/')
+
+print('The status code is ', hp.status_code)
+print('The content is ',hp.content)
